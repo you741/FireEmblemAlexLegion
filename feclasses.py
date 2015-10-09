dc5d6a45881b0800cba53164dd717a6ef52c4ecc
You Zhou <youzhouphoenix@hotmail.com>
2015-10-08 22:07:27 -0400

    Finalized Attack Methods
    Now attacks should work. I'm moving on to more commands such as item
    equipment and movement.

--- feclasses.py
+++ feclasses.py
@@ -3,9 +3,12 @@
 #Fire Emblem Classes
 
 #==============TODO==============#
-# 1. Edit all attack values      #
+#                                #
 #================================#
 
+#=========Recent Updates=========#
+# Finalized attack methods       #
+#================================#
 from random import randint
 from feweapons import *
 #parent of all units - Person
@@ -24,13 +27,23 @@ class Person:
         self.mounted = False
         self.alive = True
         self.equip = Weapon("No weapon",0,0,0,0,"")
+        self.mast1 = 0 #sword mastery
+        self.mast2 = 0 #axe mastery
+        self.mast3 = 0 #lance mastery
+        self.mast4 = 0 #bow mastery
+        self.mast5 = 0 #anima mastery
+        self.mast6 = 0 #dark mastery
+        self.mast7 = 0 #light mastery
+        self.mast8 = 0 #staff mastery
+        self.x = 0
+        self.y = 0
         self.items = []
         self.growths = growths #growths are in the following format
                           #[hp,str,dex,spd,lck,def,res]
         self.level = 1
         self.CLASS = "Person"
     def losehp(self,damage):
-        damage_t = damage - self.defense if not mag else damage - self.resistance
+        damage_t = damage
         if damage_t < 0:
             damage_t = 0
         self.hp -= damage_t
@@ -83,55 +96,31 @@ class Person:
                 self.resistance += 1
                 print("Resistance increased to",self.resistance)
             
-#Murderer Class is basic for all fighting classes
+#Murderer Class is basis for all fighting classes
 class Murderer(Person):
     def __init__(self,name,hp,stren,spd,dex,defen,lck,res=0,con=5,growths=[50,50,50,50,50,50,50]):
         super(Murderer,self).__init__(name,hp,stren,spd,dex,defen,lck,res,con,growths)
         self.CLASS = "Murderer"
-    def losehp(self,damage):
-        return super(Murderer,self).losehp(damage)
-    def gainhp(self,hp):
-        super(Murderer,self).gainhp(hp)
-    def attack(self,enemy):
-        if self.dex*2 + accuracy - enemy.spd*2 > randint(0,100):
-            damage_t = enemy.losehp(self.strength)
+    def attack(self,enemy,terr=0):
+        if self.dexterity*2 + self.luck//2 + self.equip.acc - enemy.speed*2 - enemy.luck + terr > randint(0,99):
+            damage_t = enemy.losehp(self.equip.damage(self,enemy))
             print(self.name,"attacked",enemy.name,"for",damage_t,"damage")
             if enemy.hp <= 0:
                 print(enemy.name,"died")
+        else:
+            print(self.name,"attacked",enemy.name,"but",enemy.name,"dodged")
 #-----------MAGE-------------#
 class Mage(Murderer):
     def __init__(self,name,hp,stren,spd,dex,defen,lck,res=0,con=5,growths=[50,50,50,50,50,50,50]):
         super(Mage,self).__init__(name,hp,stren,spd,dex,defen,lck,res,con,growths)
         self.CLASS = "Mage"
-    def losehp(self,damage):
-        return super(Mage,self).losehp(damage)
-    def gainhp(self,hp):
-        super(Mage,self).gainhp(hp)
-    def attack(self,enemy,accuracy=95):
-        if self.dex*2 + accuracy - enemy.spd*2 > randint(0,100):
-            damage_t = enemy.losehp(self.strength,True)
-            print(self.name,"attacked",enemy.name,"for",damage_t,"damage")
-            if enemy.hp <= 0:
-                print(enemy.name,"died")
-        else:
-            print(self.name,"attacked",enemy.name,"but",enemy.name,"dodged")
+        self.mast5 = 2
 #----------KNIGHT------------#
 class Knight(Murderer):
     def __init__(self,name,hp,stren,spd,dex,defen,lck,res=0,con=5,growths=[50,50,50,50,50,50,50]):
         super(Knight,self).__init__(name,hp,stren,spd,dex,defen,lck,res,con,growths)
         self.CLASS = "Knight"
-    def losehp(self,damage):
-        return super(Knight,self).losehp(damage)
-    def gainhp(self,hp):
-        super(Knight,self).gainhp(hp)
-    def attack(self,enemy,accuracy=80):
-        if self.dex*2 + accuracy - enemy.spd*2 > randint(0,100):
-            damage_t = enemy.losehp(self.strength,True)
-            print(self.name,"attacked",enemy.name,"for",damage_t,"damage")
-            if enemy.hp <= 0:
-                print(enemy.name,"died")
-        else:
-            print(self.name,"attacked",enemy.name,"but",enemy.name,"dodged")
+        self.mast2 = 2
 #---------MYRMIDON-----------#
 class Myrmidon(Murderer):
     def __init__(self,name,hp,stren,spd,dex,defen,lck,res=0,con=5,growths=[50,50,50,50,50,50,50]):
@@ -141,37 +130,9 @@ class Myrmidon(Murderer):
         return super(Myrmidon,self).losehp(damage)
     def gainhp(self,hp):
         super(Myrmidon,self).gainhp(hp)
-    def attack(self,enemy,accuracy=90):
-        if self.dex*2 + accuracy - enemy.spd*2 > randint(0,100):
-            if self.dex//2 > randint(0,100):
-                damage_t = enemy.losehp(self.strength*2,True)
-                print(self.name,"attacked",enemy.name,"with a critical hit for",damage_t,"damage")
-            else:
-                damage_t = enemy.losehp(self.strength,True)
-                print(self.name,"attacked",enemy.name,"for",damage_t,"damage")
-            if enemy.hp <= 0:
-                print(enemy.name,"died")
-        else:
-            print(self.name,"attacked",enemy.name,"but",enemy.name,"dodged")
 #INCOMPLETE BELOW#
 class Cavalier(Murderer):
     def __init__(self,name,hp,stren,spd,dex,defen,lck,res=0,con=5,growths=[50,50,50,50,50,50,50]):
         super(Cavalier,self).__init__(name,hp,stren,spd,dex,defen,lck,res,con,growths)
         self.CLASS = "Cavalier"
         self.mounted = True
-    def losehp(self,damage):
-        return super(Cavalier,self).losehp(damage)
-    def gainhp(self,hp):
-        super(Cavalier,self).gainhp(hp)
-    def attack(self,enemy,accuracy=90):
-        if self.dex*2 + accuracy - enemy.spd*2 > randint(0,100):
-            if self.dex//2 > randint(0,100):
-                damage_t = enemy.losehp(self.strength*2,True)
-                print(self.name,"attacked",enemy.name,"with a critical hit for",damage_t,"damage")
-            else:
-                damage_t = enemy.losehp(self.strength,True)
-                print(self.name,"attacked",enemy.name,"for",damage_t,"damage")
-            if enemy.hp <= 0:
-                print(enemy.name,"died")
-        else:
-            print(self.name,"attacked",enemy.name,"but",enemy.name,"dodged")
